1fa61a4dc500522c0036f831c8f60eb2fb12a664
rickykhulal <withrickytalk@gmail.com>
2025-06-21 10:14:32 +0545

    Update sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -12,6 +12,8 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
+import FeedbackButton from "@/components/feedback/FeedbackButton";
+
 import {
   Tooltip,
   TooltipContent,
